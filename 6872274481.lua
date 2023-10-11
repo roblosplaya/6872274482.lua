2bce241e693f383025399eaeae60f75ab9db1511
XOver1 <143820926+roblosplaya@users.noreply.github.com>
2023-10-11 00:33:33 -0500

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -3196,49 +3196,6 @@ runFunction(function()
 	})
 end)
 
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextLabel = Instance.new("TextLabel")
-
---Properties:
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.BackgroundTransparency = 1.000
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.0328467153, 0, 0.479591846, 0)
-Frame.Size = UDim2.new(0, 210, 0, 59)
-
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(-0.0639983416, 0, 3.12903237, 0)
-TextLabel.Size = UDim2.new(0, 232, 0, 61)
-TextLabel.Font = Enum.Font.Arcade
-TextLabel.Text = "Wizzware V5"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextSize = 40.000
-
--- Scripts:
-
-local function QUPV_fake_script() -- TextLabel.LocalScript 
-	local script = Instance.new('LocalScript', TextLabel)
-
-	while true do
-		wait(1)
-		script.Parent.TextColor3 = Color3.new(math.random(),math.random(),math.random())
-		wait()  --Add a wait if necessary 
-		script.Parent.TextStrokeColor3 = Color3.new(math.random(),math.random(),math.random())
-	end 
-end
-coroutine.wrap(QUPV_fake_script)()
-
 
 runFunction(function()
 	local GrappleExploit = {Enabled = false}
@@ -10712,289 +10669,46 @@ end)
 
 
 
-if game:GetService("CoreGui"):FindFirstChild("KEYS") then
-    game:GetService("CoreGui"):FindFirstChild("KEYS"):Destroy()
-end
-wait(1)
 local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Parent =  game:GetService("CoreGui")
-ScreenGui.Name = 'KEYS'
-local keystroke = Instance.new("Frame")
-local AKey = Instance.new("Frame")
-local UICorner = Instance.new("UICorner")
-local ALabel = Instance.new("TextLabel")
-local SKey = Instance.new("Frame")
-local UICorner_2 = Instance.new("UICorner")
-local SLabel = Instance.new("TextLabel")
-local DKey = Instance.new("Frame")
-local UICorner_3 = Instance.new("UICorner")
-local DLabel = Instance.new("TextLabel")
-local WKey = Instance.new("Frame")
-local UICorner_4 = Instance.new("UICorner")
-local WLabel = Instance.new("TextLabel")
-local SpaceBar = Instance.new("Frame")
-local UICorner_5 = Instance.new("UICorner")
-local SpaceBarLabel = Instance.new("TextLabel")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
 
-keystroke.Name = "keystroke"
-keystroke.Parent = ScreenGui
-keystroke.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-keystroke.BackgroundTransparency = 1.000
-keystroke.Position = UDim2.new(-0.00722312089, 0, 0.690598845, 0)
-keystroke.Size = UDim2.new(0, 285, 0, 287)
+--Properties:
 
-AKey.Name = "AKey"
-AKey.Parent = keystroke
-AKey.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-AKey.BackgroundTransparency = 0.450
-AKey.Position = UDim2.new(0.0454545468, 0, 0.536585391, 0)
-AKey.Size = UDim2.new(0, 49, 0, 49)
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-UICorner.Parent = AKey
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.Position = UDim2.new(0.00557398004, 0, 0.906122446, 0)
+Frame.Size = UDim2.new(0, 252, 0, 35)
 
-ALabel.Name = "ALabel"
-ALabel.Parent = AKey
-ALabel.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-ALabel.BackgroundTransparency = 1.000
-ALabel.Position = UDim2.new(-0.142857075, 0, 0, 0)
-ALabel.Size = UDim2.new(0, 62, 0, 50)
-ALabel.Font = Enum.Font.FredokaOne
-ALabel.Text = "A"
-ALabel.TextColor3 = Color3.fromRGB(255 ,0 ,0)
-ALabel.TextSize = 32.000
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.0702007264, 0, -0.336826861, 0)
+TextLabel.Size = UDim2.new(0, 216, 0, 58)
+TextLabel.Font = Enum.Font.FredokaOne
+TextLabel.Text = "Wizzware V5"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 40.000
 
-SKey.Name = "SKey"
-SKey.Parent = keystroke
-SKey.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-SKey.BackgroundTransparency = 0.450
-SKey.Position = UDim2.new(0.238436997, 0, 0.536585391, 0)
-SKey.Size = UDim2.new(0, 49, 0, 49)
+-- Scripts:
 
-UICorner_2.Parent = SKey
+local function IADGT_fake_script() -- TextLabel.LocalScript 
+	local script = Instance.new('LocalScript', TextLabel)
 
-SLabel.Name = "SLabel"
-SLabel.Parent = SKey
-SLabel.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-SLabel.BackgroundTransparency = 1.000
-SLabel.Position = UDim2.new(-0.122448914, 0, -0.0204081628, 0)
-SLabel.Size = UDim2.new(0, 62, 0, 50)
-SLabel.Font = Enum.Font.FredokaOne
-SLabel.Text = "S"
-SLabel.TextColor3 = Color3.fromRGB(255 ,0 ,0)
-SLabel.TextSize = 32.000
-
-DKey.Name = "DKey"
-DKey.Parent = keystroke
-DKey.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-DKey.BackgroundTransparency = 0.450
-DKey.Position = UDim2.new(0.431419432, 0, 0.540069699, 0)
-DKey.Size = UDim2.new(0, 49, 0, 49)
-
-UICorner_3.Parent = DKey
-
-DLabel.Name = "DLabel"
-DLabel.Parent = DKey
-DLabel.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-DLabel.BackgroundTransparency = 1.000
-DLabel.Position = UDim2.new(-0.142857015, 0, 0, 0)
-DLabel.Size = UDim2.new(0, 62, 0, 50)
-DLabel.Font = Enum.Font.FredokaOne
-DLabel.Text = "D"
-DLabel.TextColor3 = Color3.fromRGB(255 ,0 ,0)
-DLabel.TextSize = 32.000
-
-WKey.Name = "WKey"
-WKey.Parent = keystroke
-WKey.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-WKey.BackgroundTransparency = 0.450
-WKey.Position = UDim2.new(0.238436982, 0, 0.347000003, 0)
-WKey.Size = UDim2.new(0, 49, 0, 49)
-
-UICorner_4.Parent = WKey
-
-WLabel.Name = "WLabel"
-WLabel.Parent = WKey
-WLabel.BackgroundColor3 = Color3.fromRGB(255 ,0 ,0)
-WLabel.BackgroundTransparency = 1.000
-WLabel.Position = UDim2.new(-0.122448921, 0, -0.00204081833, 0)
-WLabel.Size = UDim2.new(0, 62, 0, 50)
-WLabel.Font = Enum.Font.FredokaOne
-WLabel.Text = "W"
-WLabel.TextColor3 = Color3.fromRGB(255 ,0 ,0)
-WLabel.TextSize = 32.000
-
-SpaceBar.Name = "SpaceBar"
-SpaceBar.Parent = keystroke
-SpaceBar.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
-SpaceBar.BackgroundTransparency = 0.450
-SpaceBar.Position = UDim2.new(0.0454544872, 0, 0.742160261, 0)
-SpaceBar.Size = UDim2.new(0, 161, 0, 31)
-
-UICorner_5.Parent = SpaceBar
-
-SpaceBarLabel.Name = "SpaceBarLabel"
-SpaceBarLabel.Parent = SpaceBar
-SpaceBarLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-SpaceBarLabel.BackgroundTransparency = 1.000
-SpaceBarLabel.Position = UDim2.new(0.304347903, 0, -0.806451619, 0)
-SpaceBarLabel.Size = UDim2.new(0, 62, 0, 50)
-SpaceBarLabel.Font = Enum.Font.FredokaOne
-SpaceBarLabel.Text = "_____"
-SpaceBarLabel.TextColor3 = Color3.fromRGB(191, 148, 228)
-SpaceBarLabel.TextSize = 32.000
-
-local UserInputService = game:GetService("UserInputService")
-
-getgenv().keys = {
-    ['Space'] = false;
-    ['W'] = false;
-    ['A'] = false;
-    ['S'] = false;
-    ['D'] = false;
-}
-
-local wButton = WKey
-
-
-local litColor = Color3.fromRGB(220, 208, 255)
-
-
-local unlitColor = Color3.fromRGB(34, 34, 34)
-
-
-UserInputService.InputBegan:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.W then
-            if wButton.BackgroundColor3 ~= litColor then
-                wButton.BackgroundColor3 = litColor
-            end
-        end
-    end
-end)
-
-
-UserInputService.InputEnded:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.W then
-            if wButton.BackgroundColor3 ~= unlitColor then
-
-                wButton.BackgroundColor3 = unlitColor
-            end
-        end
-    end
-end)
-
-
-
-local dButton = DKey
-
-
-UserInputService.InputBegan:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.D then
-            if dButton.BackgroundColor3 ~= litColor then
-            dButton.BackgroundColor3 = litColor
-            end
-        end
-    end
-end)
-
-
-UserInputService.InputEnded:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.D then
-            if dButton.BackgroundColor3 ~= unlitColor then
-            dButton.BackgroundColor3 = unlitColor
-            end
-        end
-    end
-end)
-
-
-local aButton = AKey
-
-
-UserInputService.InputBegan:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.A then
-            if aButton.BackgroundColor3 ~= litColor then
-            aButton.BackgroundColor3 = litColor
-            end
-        end
-    end
-end)
-
-
-UserInputService.InputEnded:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.A then
-            if aButton.BackgroundColor3 ~= unlitColor then
-            aButton.BackgroundColor3 = unlitColor
-            end
-        end
-    end
-end)
-
-
-
-local sButton = SKey
-
-
-
-UserInputService.InputBegan:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.S then
-            if sButton.BackgroundColor3 ~= litColor then
-                sButton.BackgroundColor3 = litColor
-            end
-        end
-    end
-end)
-
-
-UserInputService.InputEnded:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.S then
-        if sButton.BackgroundColor3 ~= unlitColor then    
-            sButton.BackgroundColor3 = unlitColor
-        end
-        end
-    end
-end)
-
-
-local spaceButton = SpaceBar
-
-UserInputService.InputBegan:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.Space then
-        if spaceButton.BackgroundColor ~= litColor then
-            spaceButton.BackgroundColor3 = litColor
-        end
-        end
-    end
-end)
-
-
-UserInputService.InputEnded:Connect(function(input, gameProcessed)
-    if not gameProcessed then
-        
-        if input.KeyCode == Enum.KeyCode.Space then
-        if spaceButton.BackgroundColor ~= unlitColor then
-            spaceButton.BackgroundColor3 = unlitColor
-        end
-        end
-    end
-end)
+	while true do
+		wait(0.2)
+		script.Parent.TextColor3 = Color3.new(math.random(),math.random(),math.random())
+		wait()  --Add a wait if necessary 
+		script.Parent.TextStrokeColor3 = Color3.new(math.random(),math.random(),math.random())
+	end 
+end
+coroutine.wrap(IADGT_fake_script)()
 
 
 
