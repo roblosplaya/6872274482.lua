e5a0b800af502c6dcb762c134a18e2ee6c919289
XOver1 <143820926+roblosplaya@users.noreply.github.com>
2023-10-10 20:40:51 -0500

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -11575,7 +11575,7 @@ FourBigballs = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].Creat
 workspace.CurrentCamera.Viewmodel.ChildAdded:Connect(function(x)
     if x and x:FindFirstChild("Handle") then
         if string.find(x.Name:lower(), 'scythe , sword') then
-            x.Handle.Material = "ForceField"
+            x.Handle.Material = "Neon"
             x.Handle.MeshId = "rbxassetid://13471207377"
             x.Handle.BrickColor = BrickColor.new("Hot pink")
         end
