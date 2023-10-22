085b5f469873421708a80b92928e5768a057ab80
XOver1 <143820926+roblosplaya@users.noreply.github.com>
2023-10-22 14:25:58 -0500

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -10805,23 +10805,36 @@ runFunction(function()
 end)
 
 runFunction(function()
-    local VampireExploit = {Enabled = false}
-    VampireExploit = GuiLibrary.ObjectsThatCanBeSaved.WizzwareWindow.Api.CreateOptionsButton({
-        Name = "VampireExploit",
-        Function = function(callback)
-            if callback then 
-                task.spawn(function()
-                    table.insert(VampireExploit.Connections, lplr.CharacterAdded:Connect(function()
-                        task.wait(1)
-                        bedwars.ClientHandler:Get("CursedCoffinApplyVampirism"):SendToServer({player = lplr})
-                    end))
-                    bedwars.ClientHandler:Get("CursedCoffinApplyVampirism"):SendToServer({player = lplr}) 
-                end)
-            end
-        end
-    })
+local VampireExploit = {Enabled = false}
+VampireExploit = GuiLibrary.ObjectsThatCanBeSaved.WizzwareWindow.Api.CreateOptionsButton({
+Name = "VampireExploit",
+Function = function(callback)
+if callback then
+task.spawn(function()
+table.insert(VampireExploit.Connections, lplr.CharacterAdded:Connect(function()
+task.wait(1)
+local players = game:GetService("Players")
+local lplr = players.LocalPlayer
+local replicatedstorage = game:GetService("ReplicatedStorage")
+local vampireRemote = replicatedstorage.rbxts_include.node_modules["@rbxts"].net.out._NetManaged:WaitForChild("CursedCoffinApplyVampirism")
+local function vampirefunc(plr)
+repeat task.wait() until plr:GetAttribute("PlayerConnected") and plr.Character and plr.Character:FindFirstChild("Head")
+plr.CharacterAdded:Connect(function()
+task.wait(1)
+vampireRemote:FireServer({player = plr})
 end)
-
+vampireRemote:FireServer({player = plr})
+end
+for i,v in players:GetPlayers() do
+if v ~= lplr then
+task.spawn(vampirefunc, v)
+end
+end
+players.PlayerAdded:Connect(vampirefunc)
+end
+end
+})
+end)
 runFunction(function()
     local Disabler = {Enabled = false}
     Disabler = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api.CreateOptionsButton({
